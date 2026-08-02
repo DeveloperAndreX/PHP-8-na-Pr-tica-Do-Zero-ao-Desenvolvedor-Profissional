88daec7869e1fd0d13d1f40302965d18f5eab28a
andre <andredeveloper45@gmail.com>
2026-08-02 13:53:54 -0300

    update this code

--- index2.php
+++ index2.php
@@ -1,12 +1,12 @@
 <?php
+
 //Arquivo responsavel pela inicialização do sistema
-
+declare(strict_types = 1);
 include_once '../PHP_8_Na_Pratica_Zero_ao_Profissional/sistema/helpers.php';
-
 require_once 'sistema/configuracao.php';
 include_once './sistema/helpers.php';
 
-$texto = 1.2;
+$texto = "Mostra um texto de um paragrafo";
 
 //Tipos de Dados em PHP
 $string = 'texto';
