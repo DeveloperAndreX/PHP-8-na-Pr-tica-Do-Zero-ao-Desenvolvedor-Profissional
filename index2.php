a6e6ad769ce06b669d1a8ab4cebcc75f18195e2e
andre <andredeveloper45@gmail.com>
2026-08-03 16:20:26 -0300

    update this code operador ternario

--- index2.php
+++ index2.php
@@ -6,11 +6,6 @@ include_once '../PHP_8_Na_Pratica_Zero_ao_Profissional/sistema/helpers.php';
 require_once 'sistema/configuracao.php';
 include_once './sistema/helpers.php';
 
-$texto = "Texto para resumir";
-// echo resumirTexto($texto, 100,'','');
-echo receberSalario(100.00,0.00);
-echo "<br>";
-// echo receberSalario(100.00, 0.00);
-// echo "<hr>";
+
 
 
