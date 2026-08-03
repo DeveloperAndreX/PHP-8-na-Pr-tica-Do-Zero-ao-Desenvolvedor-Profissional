577a88302afcab3bc68ff48b0db0d9ae5a5427ae
andre <andredeveloper45@gmail.com>
2026-08-03 16:52:40 -0300

    update this code

--- index2.php
+++ index2.php
@@ -1,27 +1,30 @@
 <?php
 //Arquivo responsavel pela inicialização do sistema
-
-
+declare(strict_types = 1);
 include_once '../PHP_8_Na_Pratica_Zero_ao_Profissional/sistema/helpers.php';
 require_once 'sistema/configuracao.php';
 include_once './sistema/helpers.php';
 
 (int)$valor = 5;
-if(isset($valor)):
+(float)$valor1 = 5.5;
+if (isset($valor,$valor1)):
 
     /**
      * @author Andre Panizza
-     * @param
+     * @var int $valor A variavel e validada no começo, porem o valor inicial sempre será inteiro
+     * 
      */
-
+    if ($valor AND $valor1) {
+        echo $valor;
+    } else {
+        echo 0;
+    }
+         // usando um ternario com a variavel $valor1:
+         echo "<br><br>";
+         echo ($valor1 ? $valor1 : 0);
+         
 else:
-    echo strtoupper("<b> a variavel não foi definida corretamente! </b>");
-    unset($valor); // Destroi a variavel;
+    echo strtoupper("<b> A variavel não foi definida corretamente! </b>");
+    unset($valor, $valor1); // Destroi a variaveis
     exit(0);
 endif;
-
-
-
-
-
-?>
