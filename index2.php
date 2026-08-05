15d08b056f10f59237b344f22eba3cc43e0a03f1
andre <andredeveloper45@gmail.com>
2026-08-04 22:57:52 -0300

    update this code

--- index2.php
+++ index2.php
@@ -5,29 +5,5 @@
 require_once __DIR__ . '/sistema/configuracao.php';
 include_once __DIR__ . '/sistema/helpers.php';
 
-(string)$data = date('d/m/Y H:i:s');
-echo $data;
 
-
-
-(int)$month = 8;
-(int)$year = 2026;
-(int)$day = 4;
-
-echo "<hr>";
-if (isset($month, $year, $day)):
-    $verifyDate = checkdate($day, $month, $year);
-    echo ucfirst(" <b> retorna: </b>  ") . "<br>";
-    echo "Tipo de Dado e:" . gettype($verifyDate) . "<br>";
-    var_dump($verifyDate);
-
-    // exemplos da documentação:
-    echo "<hr>";
-
-    var_dump(checkdate(12, 31, 2000)); //bool (true);
-    var_dump(checkdate(2, 29, 2001)); //bool(false);
-
-else:
-    echo "FATAL ERROR";
-
-endif;
+?>
