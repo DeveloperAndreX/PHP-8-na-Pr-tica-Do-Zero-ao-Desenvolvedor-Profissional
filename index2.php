6771949d6cfde772477431ab1f71bdf4a97e03dd
andre <andredeveloper45@gmail.com>
2026-08-04 22:33:57 -0300

    update this code

--- index2.php
+++ index2.php
@@ -5,6 +5,25 @@
 require_once __DIR__ . '/sistema/configuracao.php';
 include_once __DIR__ . '/sistema/helpers.php';
 
+(int)$month = 8 ;
+(int)$year = 2026;
+(int)$day = 4;
+
+(string)$data = date('d/m/Y H:i:s');
+echo $data;
+
+if(isset($date1)):
+$verifyDate = checkdate($month, $year, $day);
+echo " retorna: " . $verifyDate . "<br>";
+endif;
+
+
+
+
+
+
+
+
 
 
 
--- sistema/configuracao.php
+++ sistema/configuracao.php
@@ -1,13 +1,10 @@
 <?php
-// Arquivo de configuração.
-
-
 // sistema/configuracao.php
 
-// Define o fuso horário padrão do Brasil
+// Define o fuso horário padrão do Brasil:
 date_default_timezone_set('America/Sao_Paulo');
 
-// Define constantes para o sistema
+// Define constantes para o sistema:
 define('SITE_NOME', 'Meu Sistema PHP 8');
 define('URL_BASE', 'http://localhost/projeto/');
 
